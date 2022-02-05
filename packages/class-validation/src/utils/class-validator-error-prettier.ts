4d740dd0857f8ae9561c922f0fe8557c6de31a7e
Ben Ryder <dev@benryder.me>
2022-02-05 12:25:53 +0000

    fix(class-validation): ensuring error prettier works for arrays of errors

--- packages/class-validation/src/utils/class-validator-error-prettier.ts
+++ packages/class-validation/src/utils/class-validator-error-prettier.ts
@@ -2,11 +2,23 @@ import { ValidationError } from "class-validator";
 
 /**
  * Prettify errors returned by class-validator.
- * @param err
+ * @param validationError
  */
-export function classValidatorErrorPrettier(err: ValidationError): string|null {
-    if (err.constraints) {
-        return Object.values(err.constraints).join(", ");
+export function classValidatorErrorPrettier(validationError: ValidationError|ValidationError[]): string[]|string|null {
+    if (Array.isArray(validationError)) {
+        const prettifiedErrors = [];
+
+        for (const error of validationError) {
+            if (error.constraints) {
+                prettifiedErrors.push(Object.values(error.constraints).join(", "));
+            }
+        }
+
+        return prettifiedErrors.length > 0 ? prettifiedErrors: null;
     }
+    else if (validationError.constraints) {
+        return Object.values(validationError.constraints).join(", ");
+    }
+
     return null;
 }
