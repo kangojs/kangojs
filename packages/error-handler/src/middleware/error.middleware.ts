d1c8071eae961bd68e2f69c126745d051f611731
Ben-Ryder <dev@benryder.me>
2022-03-10 20:13:12 +0000

    fix(error-handler): simplifying useErrorHandlerMiddleware config

--- packages/error-handler/src/middleware/error.middleware.ts
+++ packages/error-handler/src/middleware/error.middleware.ts
@@ -1,15 +1,11 @@
 import { Application, NextFunction, Request, Response } from 'express';
 import { ErrorHandler, ErrorHandlerConfig } from '../handlers/error-handler';
 
-export interface ErrorHandlerMiddlewareConfig {
-  errorHandlerConfig: ErrorHandlerConfig
-}
-
 /**
  * A middleware to handle all thrown errors via the main error handler.
  */
-export function useErrorHandlerMiddleware(app: Application, config?: ErrorHandlerMiddlewareConfig) {
-    const errorHandler = new ErrorHandler(config?.errorHandlerConfig);
+export function useErrorHandlerMiddleware(app: Application, config?: ErrorHandlerConfig) {
+    const errorHandler = new ErrorHandler(config);
 
     async function errorHandlerMiddleware(err: Error, req: Request, res: Response, next: NextFunction) {
         await errorHandler.handleError(err, res);
