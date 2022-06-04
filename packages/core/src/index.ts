4b59c39839c425b1ca78367cd9a82fdf620855e1
Ben Ryder <dev@benryder.me>
2022-06-04 17:56:11 +0100

    fix: adding missing exports from core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,6 +6,20 @@ export * from "./decorators/metadata-keys";
 export * from "./decorators/middleware.decorator";
 export * from "./decorators/route.decorator";
 
+export * from "./enums/http-status-codes";
+export * from "./enums/http-methods";
+
+export * from "./errors/base.error";
+export * from "./errors/system.error";
+export * from "./errors/user.error";
+export * from "./errors/access/access.error";
+export * from "./errors/access/access-denied.error";
+export * from "./errors/access/access-forbidden.error";
+export * from "./errors/resource/resource.error";
+export * from "./errors/resource/resource-not-found.error";
+export * from "./errors/resource/resource-relationship.error";
+export * from "./errors/resource/resource-not-unique.error";
+
 export * from "./middleware/common.middleware";
 export * from "./middleware/gnu-terry-pratchett";
 export * from "./middleware/malformed-requests";
@@ -21,4 +35,7 @@ export * from "./types/route/route-definition";
 export * from "./types/route/route-metadata";
 
 export * from "./utils/dependency-container";
+export * from "./utils/error-handler";
+export * from "./utils/error-http-mappings";
+export * from "./utils/error-response-manager";
 export * from "./utils/logger";
